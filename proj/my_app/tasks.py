e32b40b22fe4bc1a56ea447d7b9d7cbac2a96f9b
aFraley <humanimalmammal@webtv.net>
2016-06-22 12:02:27 -0500

    Initial commit

--- proj/my_app/tasks.py
+++ proj/my_app/tasks.py
@@ -18,10 +18,12 @@ api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
 @shared_task(name='get_tweets')
 def get_tweets():
     """Get some tweets from the twitter api and store them to the db."""
-    t_tweets = Tweet.objects.all().values('tweet_id')
+    db_tweets = Tweet.objects.all()
+    max_id = min([tweet.tweet_id for tweet in db_tweets])
     tweets = api.search(
         q='#python',
-        count=15
+        max_id=max_id,
+        count=100
     )
     tweets_date = [tweet.created_at for tweet in tweets]
     tweets_id = [tweet.id for tweet in tweets]
